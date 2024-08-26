0e46d5d4f168bce555eed81a7a35397e73da11b1
+ <+>
2024-08-26 20:04:31 +0300

    feat:login and signup views

--- server/bericks_backend/users/views.py
+++ server/bericks_backend/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 @api_view(['POST'])
 def register(request):
@@ -13,3 +14,15 @@ def register(request):
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
